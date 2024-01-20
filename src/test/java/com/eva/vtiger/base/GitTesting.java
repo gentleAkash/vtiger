2fead3ba143e0a72b1010a15683debe62c311995
gentleAkash <palakash70114@gmail.com>
2024-01-20 23:26:24 +0530

    method three

--- src/test/java/com/eva/vtiger/base/GitTesting.java
+++ src/test/java/com/eva/vtiger/base/GitTesting.java
@@ -3,6 +3,9 @@ package com.eva.vtiger.base;
 public class GitTesting {
 	public static void main(String[] args) {
 		method1();
+		method2();
+		method3();
+		
 		System.out.println("this is main method");
 	}
 
@@ -10,4 +13,14 @@ public class GitTesting {
 		System.out.println("method one");
 		
 	}
+	public static void method2() {
+		System.out.println("method two");
+		
+	}
+
+	public static void method3() {
+		System.out.println("method three");
+		
 }
+}
+
